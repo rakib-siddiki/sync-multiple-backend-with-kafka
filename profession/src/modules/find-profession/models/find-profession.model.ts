3491bad9fe051ca40d8d27ded32dceead0b5deea
rakib-siddiki <rmrakib901@gmail.com>
2025-07-09 16:00:10 +0600

    feat: define IFindProfession interface and update findProfessionSchema with new fields

--- profession/src/modules/find-profession/models/find-profession.model.ts
+++ profession/src/modules/find-profession/models/find-profession.model.ts
@@ -1,11 +1,32 @@
 import { model, Schema } from "mongoose";
+import { IFindProfession } from "../types/find-profession.type";
 
-const findProfessionSchema = new Schema(
+const findProfessionSchema = new Schema<IFindProfession>(
   {
     _id: { type: Schema.Types.ObjectId, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    branch: [{ type: Schema.Types.ObjectId, ref: "Branch" }],
+    type: {
+      type: String,
+      enum: ["Practitioner", "Organization"],
+      required: true,
+    },
+    orgId: { type: Schema.Types.ObjectId, ref: "Organization", default: null },
+    pracId: { type: Schema.Types.ObjectId, ref: "Practitioner", default: null },
+    status: { type: String, default: "" },
+    username: { type: String, required: true },
+    businessUrl: { type: String, default: "" },
+    practitionerName: { type: String, default: "" },
+    orgName: { type: String, default: "" },
+    photoUrl: { type: String, default: "" },
+    category: { type: String, default: "" },
+    subCategory: { type: [String], default: [] },
+    listOfDegrees: { type: [String], default: [] },
+    ranking: { type: Number, default: 0 },
+    rating: { type: Number, default: 0 },
+    totalAppointment: { type: Number, default: 0 },
+    zone: { type: [String], default: [] },
+    city: { type: [String], default: [] },
+    address: { type: [String], default: [] },
+    areaOfPractice: { type: String, default: "" },
   },
   {
     timestamps: true,
---
+++ profession/src/modules/find-profession/types/find-profession.type.ts
@@ -0,0 +1,24 @@
+import type { ObjectId } from "mongoose";
+
+export interface IFindProfession {
+  _id: ObjectId;
+  type: "Practitioner" | "Organization";
+  orgId: ObjectId | null;
+  pracId: ObjectId | null;
+  status?: string;
+  username: string;
+  businessUrl: string;
+  practitionerName: string;
+  orgName: string;
+  photoUrl?: string;
+  category: string;
+  subCategory: string[];
+  listOfDegrees: string[];
+  ranking: number;
+  rating: number;
+  totalAppointment: number;
+  zone: string[];
+  city: string[];
+  address: string[];
+  areaOfPractice: string;
+}
